348ed44595c2bb0f13d90375f70be815fafb787f
Thúlio Noslen <thulionoslen_@hotmail.com>
2025-03-04 17:20:50 -0300

    fixed annotation messing with function signature

--- custom_profiler.py
+++ custom_profiler.py
@@ -2,9 +2,6 @@ import cProfile
 import functools
 import io
 import pstats
-from collections.abc import Callable
-
-from config import Config as c
 
 _PROFILER = cProfile.Profile(subcalls=False, builtins=False)
 
@@ -12,14 +9,31 @@ _FUNCS = []
 
 # TODO: try to implement as class again
 
+DEACTIVATE = False
 
-def timeit(
-            func: Callable,
-            deactivate: bool = not c.PROFILE_EXEC_TIMES
-        ) -> Callable:
+
+# def timeit(func, deactivate: bool = False):
+#     @functools.wraps(func)
+#     def run_func(*args, **kwargs):
+#         if deactivate:
+#             return func(*args, **kwargs)
+#         try:
+#             _PROFILER.enable()
+#         except ValueError:
+#             return func(*args, **kwargs)
+#         try:
+#             return func(*args, **kwargs)
+#         finally:
+#             _PROFILER.disable()
+#     if func.__name__ not in _FUNCS:
+#         _FUNCS.append(func.__name__)
+#     return run_func
+
+
+def timeit(func):
     @functools.wraps(func)
     def run_func(*args, **kwargs):
-        if deactivate:
+        if DEACTIVATE:
             return func(*args, **kwargs)
         try:
             _PROFILER.enable()
@@ -34,11 +48,8 @@ def timeit(
     return run_func
 
 
-def printit(
-            clear: bool = False,
-            deactivate: bool = not c.PROFILE_EXEC_TIMES
-        ) -> None:
-    if not deactivate:
+def printit(clear: bool = False) -> None:
+    if not DEACTIVATE:
         sio = io.StringIO()
         ps = pstats.Stats(_PROFILER, stream=sio)
         ps.strip_dirs().sort_stats(pstats.SortKey.STDNAME).print_stats('|'.join(_FUNCS))
