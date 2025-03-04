589cd2367b0617e255abea3469aeeccb13d16d85
Thúlio Noslen <thulionoslen_@hotmail.com>
2025-03-04 17:22:07 -0300

    better isolation

--- inference_runner.py
+++ inference_runner.py
@@ -1,3 +1,5 @@
+from enum import Enum, auto
+
 import cv2
 import numpy as np
 from mediapipe import Image, ImageFormat
@@ -8,43 +10,74 @@ from mediapipe.tasks.python.vision.face_landmarker import FaceLandmarker, FaceLa
 from mediapipe.tasks.python.vision.hand_landmarker import HandLandmarker, HandLandmarkerOptions
 from mediapipe.tasks.python.vision.image_segmenter import ImageSegmenter, ImageSegmenterOptions
 
-from config import Config as c
 from custom_profiler import timeit
+from data import Location
+
+type Detections = list[tuple[tuple[int, int, int, int], np.ndarray[int]]]
+type Masks = tuple[np.ndarray[int], list[np.ndarray[float]]]
+
+# type Location = tuple[int | float, int | float, int | float, int | float, int | float, int | float]
+
+
+class ModelType(Enum):
+    FACE_DETECTOR = auto()
+    FACE_LANDMARKER = auto()
+    HAND_LANDMARKER = auto()
+    PERSON_SEGMENTER = auto()
+
+
+FACE_DETECTION_NOSE_INDEX = 2
+FACE_LANDMARKS_NOSE_INDEX = 4
+FACE_LANDMARKS_FOREHEAD_INDEX = 151 # 10
+FACE_LANDMARKS_CHEEK_INDEX = 330 # 101
+FACE_LANDMARKS_EYEBROW_INDEX = 337 # 108
+HAND_LANDMARKS_WRIST_INDEX = 0
+HAND_LANDMARKS_MIDDLE_INDEX = 9
+
+FACE_CHEEK_CONFIG = (ModelType.FACE_LANDMARKER, [FACE_LANDMARKS_CHEEK_INDEX], (-0.05, -0.05, 0.15, 0.05))
+FACE_EYEBROW_CONFIG = (ModelType.FACE_LANDMARKER, [FACE_LANDMARKS_EYEBROW_INDEX], (-0.10, -0.15, 0.25, 0.00))
+FACE_FOREHEAD_CONFIG = (ModelType.FACE_LANDMARKER, [FACE_LANDMARKS_FOREHEAD_INDEX], (-0.00, -0.10, 0.20, 0.05))
+HAND_WRIST_CONFIG = (ModelType.HAND_LANDMARKER, [HAND_LANDMARKS_WRIST_INDEX], (-0.10, -0.10, 0.10, 0.10))
+HAND_PALM_CONFIG = (ModelType.HAND_LANDMARKER, [HAND_LANDMARKS_WRIST_INDEX, HAND_LANDMARKS_MIDDLE_INDEX], (-0.10, -0.10, 0.10, 0.10))
 
 
 class InferenceRunner:
 
-    def __init__(
-                self,
-                face_detector_path: str | None = 'models/blaze_face_short_range.tflite',
-                face_landmarker_path: str | None = 'models/face_landmarker.task',
-                hand_landmarker_path: str | None = 'models/hand_landmarker.task',
-                person_segmenter_path: str | None = 'models/selfie_multiclass.tflite',
-                running_mode: VisionTaskRunningMode = c.RUNNING_MODE
-            ) -> None:
+    @timeit
+    def __init__(self,
+                 face_detection_enabled: bool = False,
+                 face_landmarks_enabled: bool = True,
+                 hand_landmarks_enabled: bool = True,
+                 person_segmentation_enabled: bool = False,
+                 face_detector_path: str | None = 'models/blaze_face_short_range.tflite',
+                 face_landmarker_path: str | None = 'models/face_landmarker.task',
+                 hand_landmarker_path: str | None = 'models/hand_landmarker.task',
+                 person_segmenter_path: str | None = 'models/selfie_multiclass.tflite',
+                 roi_configs: list[tuple[ModelType, list[int], tuple[float, float, float, float]]] = (FACE_FOREHEAD_CONFIG, HAND_PALM_CONFIG),
+                 running_mode: VisionTaskRunningMode = VisionTaskRunningMode.VIDEO) -> None:
+        self.face_detection_enabled = face_detection_enabled
+        self.face_landmarks_enabled = face_landmarks_enabled
+        self.hand_landmarks_enabled = hand_landmarks_enabled
+        self.person_segmentation_enabled = person_segmentation_enabled
+        self.roi_configs = roi_configs
         self.running_mode = running_mode
-        if c.FACE_DETECTION_ENABLED and face_detector_path is not None:
+        if self.face_detection_enabled and face_detector_path is not None:
             face_detector_options = FaceDetectorOptions(BaseOptions(face_detector_path), self.running_mode)
             self.face_detector = FaceDetector.create_from_options(face_detector_options)
-        if c.FACE_LANDMARKS_ENABLED and face_landmarker_path is not None:
+        if self.face_landmarks_enabled and face_landmarker_path is not None:
             face_landmarker_options = FaceLandmarkerOptions(BaseOptions(face_landmarker_path), self.running_mode)
             self.face_landmarker = FaceLandmarker.create_from_options(face_landmarker_options)
-        if c.HAND_LANDMARKS_ENABLED and hand_landmarker_path is not None:
+        if self.hand_landmarks_enabled and hand_landmarker_path is not None:
             hand_landmarker_options = HandLandmarkerOptions(BaseOptions(hand_landmarker_path), self.running_mode)
             self.hand_landmarker = HandLandmarker.create_from_options(hand_landmarker_options)
-        if c.PERSON_SEGMENTATION_ENABLED and person_segmenter_path is not None:
+        if self.person_segmentation_enabled and person_segmenter_path is not None:
             person_segmenter_options = ImageSegmenterOptions(BaseOptions(person_segmenter_path), self.running_mode, True, True)
             self.person_segmenter = ImageSegmenter.create_from_options(person_segmenter_options)
 
     @timeit
-    def _run_face_detector(
-                self,
-                frame_mp: Image,
-                timestamp_ms: int | None = None,
-                sort_largest: bool = True
-            ) -> tuple[c.ModelType, c.Detections]:
+    def _run_face_detector(self, frame_mp: Image, timestamp_ms: int | None = None, sort_largest: bool = True) -> tuple[ModelType, Detections]:
         face_detections = []
-        if c.FACE_DETECTION_ENABLED:
+        if self.face_detection_enabled:
             if self.running_mode == VisionTaskRunningMode.IMAGE:
                 face_detection_results = self.face_detector.detect(frame_mp)
             elif self.running_mode == VisionTaskRunningMode.VIDEO:
@@ -62,17 +95,12 @@ class InferenceRunner:
                 face_detections.append((bbox, points))
             if sort_largest:
                 face_detections = sorted(face_detections, key=lambda e: (e[0][2] - e[0][0]) * (e[0][3] - e[0][1]), reverse=True)
-        return c.ModelType.FACE_DETECTOR, face_detections
+        return ModelType.FACE_DETECTOR, face_detections
 
     @timeit
-    def _run_face_landmarker(
-                self,
-                frame_mp: Image,
-                timestamp_ms: int | None = None,
-                sort_largest: bool = True
-            ) -> tuple[c.ModelType, c.Detections]:
+    def _run_face_landmarker(self, frame_mp: Image, timestamp_ms: int | None = None, sort_largest: bool = True) -> tuple[ModelType, Detections]:
         face_landmarks = []
-        if c.FACE_LANDMARKS_ENABLED:
+        if self.face_landmarks_enabled:
             if self.running_mode == VisionTaskRunningMode.IMAGE:
                 face_landmarker_results = self.face_landmarker.detect(frame_mp)
             elif self.running_mode == VisionTaskRunningMode.VIDEO:
@@ -87,17 +115,12 @@ class InferenceRunner:
                 face_landmarks.append((bbox, points))
             if sort_largest:
                 face_landmarks = sorted(face_landmarks, key=lambda e: (e[0][2] - e[0][0]) * (e[0][3] - e[0][1]), reverse=True)
-        return c.ModelType.FACE_LANDMARKER, face_landmarks
+        return ModelType.FACE_LANDMARKER, face_landmarks
 
     @timeit
-    def _run_hand_landmarker(
-                self,
-                frame_mp: Image,
-                timestamp_ms: int | None = None,
-                sort_largest: bool = True
-            ) -> tuple[c.ModelType, c.Detections]:
+    def _run_hand_landmarker(self, frame_mp: Image, timestamp_ms: int | None = None, sort_largest: bool = True) -> tuple[ModelType, Detections]:
         hand_landmarks = []
-        if c.HAND_LANDMARKS_ENABLED:
+        if self.hand_landmarks_enabled:
             if self.running_mode == VisionTaskRunningMode.IMAGE:
                 hand_landmarker_results = self.hand_landmarker.detect(frame_mp)
             elif self.running_mode == VisionTaskRunningMode.VIDEO:
@@ -112,17 +135,13 @@ class InferenceRunner:
                 hand_landmarks.append((bbox, points))
             if sort_largest:
                 hand_landmarks = sorted(hand_landmarks, key=lambda e: (e[0][2] - e[0][0]) * (e[0][3] - e[0][1]), reverse=True)
-        return c.ModelType.HAND_LANDMARKER, hand_landmarks
+        return ModelType.HAND_LANDMARKER, hand_landmarks
 
     @timeit
-    def _run_person_segmenter(
-                self,
-                frame_mp: Image,
-                timestamp_ms: int | None = None
-            ) -> tuple[c.ModelType, c.Masks]:
+    def _run_person_segmenter(self, frame_mp: Image, timestamp_ms: int | None = None) -> tuple[ModelType, Masks]:
         class_mask = np.array([])
         conf_masks = []
-        if c.PERSON_SEGMENTATION_ENABLED:
+        if self.person_segmentation_enabled:
             if self.running_mode == VisionTaskRunningMode.IMAGE:
                 person_segmenter_results = self.person_segmenter.segment(frame_mp)
             elif self.running_mode == VisionTaskRunningMode.VIDEO:
@@ -131,18 +150,40 @@ class InferenceRunner:
                 raise NotImplementedError
             class_mask = person_segmenter_results.category_mask.numpy_view()
             conf_masks = [conf_mask.numpy_view() for conf_mask in person_segmenter_results.confidence_masks]
-        return c.ModelType.PERSON_SEGMENTER, (class_mask, conf_masks)
+        return ModelType.PERSON_SEGMENTER, (class_mask, conf_masks)
 
     @timeit
-    def run_pipe(
-                self,
-                frame: cv2.typing.MatLike,
-                timestamp_ms: int | None = None,
-                sort_largest: bool = True,
-            ) -> list[tuple[c.ModelType, c.Detections | c.Masks]]:
+    def run_pipe(self, frame: cv2.typing.MatLike, timestamp_ms: int | None = None,
+                 sort_largest: bool = True) -> list[tuple[ModelType, Detections | Masks]]:
         frame_mp = Image(image_format=ImageFormat.SRGB, data=cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
         face_detector_results = self._run_face_detector(frame_mp, timestamp_ms, sort_largest)
         face_landmarker_results = self._run_face_landmarker(frame_mp, timestamp_ms, sort_largest)
         hand_landmarker_results = self._run_hand_landmarker(frame_mp, timestamp_ms, sort_largest)
         person_segmenter_results = self._run_person_segmenter(frame_mp, timestamp_ms)
         return face_detector_results, face_landmarker_results, hand_landmarker_results, person_segmenter_results
+
+    @timeit
+    def calc_rois(self, inference_results: list[tuple[ModelType, Detections | Masks]],
+                  roi_configs: tuple[tuple] = (FACE_FOREHEAD_CONFIG, HAND_PALM_CONFIG)) -> list[Location]:
+        _, (_, face_landmarks), (_, hand_landmarks), _ = inference_results
+        rois = []
+        for model_type, landmark_indices, (left_m, top_m, right_m, bottom_m) in roi_configs:
+            if model_type is ModelType.FACE_LANDMARKER:
+                landmarks = face_landmarks
+            elif model_type is ModelType.HAND_LANDMARKER:
+                landmarks = hand_landmarks
+            else:
+                raise NotImplementedError
+            if len(landmarks) > 0:
+                bbox, points = landmarks[0]
+                pp = np.squeeze(np.mean([points[i] for i in landmark_indices], axis=0))
+                x, y = pp.round().astype(int) if not np.isnan(pp).any() else pp
+                x_0 = int(round(x + left_m * (bbox[2] - bbox[0])))
+                y_0 = int(round(y + top_m * (bbox[3] - bbox[1])))
+                x_1 = int(round(x + right_m * (bbox[2] - bbox[0])))
+                y_1 = int(round(y + bottom_m * (bbox[3] - bbox[1])))
+                roi = (x, y, x_0, y_0, x_1, y_1)
+            else:
+                roi = (np.nan,) * 6
+            rois.append(roi)
+        return rois
--- video_reader.py
+++ video_reader.py
@@ -3,21 +3,30 @@ import time
 import cv2
 import numpy as np
 
-from config import Config as c
 from custom_profiler import timeit
 
 
 class VideoReader:
 
-    def __init__(
-                self,
-                path: int | str,
-                target_res: tuple[int, int] | None = None
-            ) -> None:
+    def __init__(self, path: int | str, target_res: tuple[int, int] | None = None) -> None:
         self.path = path
         self.target_res = target_res
+        self.auto_adjust_time = 5
+        self.optimal_focus = 65.0 # 50.0
+        self.props = [
+            (cv2.CAP_PROP_FOCUS, 5, 'cv2.CAP_PROP_FOCUS'), # 50 [0, 250]
+            (cv2.CAP_PROP_WB_TEMPERATURE, 100, 'cv2.CAP_PROP_WB_TEMPERATURE'), # 4783 [2000, 6500]
+            (cv2.CAP_PROP_BRIGHTNESS, 4, 'cv2.CAP_PROP_BRIGHTNESS'), # 128 [0, 255]
+            (cv2.CAP_PROP_CONTRAST, 4, 'cv2.CAP_PROP_CONTRAST'), # 128 [0, 255]
+            (cv2.CAP_PROP_SATURATION, 4, 'cv2.CAP_PROP_SATURATION'), # 128 [0, 255]
+            (cv2.CAP_PROP_EXPOSURE, 32, 'cv2.CAP_PROP_EXPOSURE'), # 128 [0, 255]
+            (cv2.CAP_PROP_GAIN, 4, 'cv2.CAP_PROP_GAIN'), # 31 []
+        ]
+        self.prop_idx = 0
         self.cap = cv2.VideoCapture(path)
-        self.cap.read()
+        read, _ = self.cap.read()
+        if not read:
+            raise RuntimeWarning
         if isinstance(self.path, int):
             self.cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter.fourcc(*'MJPG'))
             if self.target_res is not None:
@@ -30,25 +39,13 @@ class VideoReader:
             self.auto_adjust = False
         self.timestamp_ref = time.time()
 
-    # TODO: implement get prop and set prop
-    #       or rather inc prop and dec prop
-    #       to get it out of bp.py
-
-    @timeit
-    def auto_adjust_props(
-                self,
-                enable: bool
-            ) -> None:
+    def auto_adjust_props(self, enable: bool) -> None:
         self.cap.set(cv2.CAP_PROP_AUTOFOCUS, int(enable))
         self.cap.set(cv2.CAP_PROP_AUTO_WB, 2 * int(enable) + 1)
         self.cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 2 * int(enable) + 1)
 
     @timeit
-    def read_frame(
-                self,
-                crop_portrait: bool = False,
-                flip_horizontally: bool = False
-            ) -> tuple[bool, cv2.typing.MatLike, float]:
+    def read_frame(self, crop_portrait: bool = False, flip_horizontally: bool = False) -> tuple[bool, cv2.typing.MatLike, float]:
         if isinstance(self.path, int):
             timestamp = time.time() - self.timestamp_ref
         else:
@@ -64,7 +61,23 @@ class VideoReader:
                 frame = frame[:, left:right, :]
             if flip_horizontally:
                 frame = cv2.flip(frame, 1)
-        if timestamp >= c.EXPOSURE_ADJUSTMENT_TIME and self.auto_adjust:
+        if timestamp >= self.auto_adjust_time and self.auto_adjust:
             self.auto_adjust_props(False)
             self.auto_adjust = False
         return read, frame, timestamp
+
+    def prop_control(self, key: int) -> None:
+        prop_id, inc_value, prop_name = self.props[self.prop_idx]
+        if ord('0') <= key <= ord('9'):
+            if key == ord('8'):
+                self.cap.set(prop_id, self.cap.get(prop_id) + inc_value)
+            elif key == ord('2'):
+                self.cap.set(prop_id, self.cap.get(prop_id) - inc_value)
+            elif key == ord('4'):
+                self.prop_idx = (self.prop_idx - 1) % len(self.props)
+            elif key == ord('6'):
+                self.prop_idx = (self.prop_idx + 1) % len(self.props)
+            elif key == ord('0'):
+                self.cap.set(cv2.CAP_PROP_FOCUS, self.optimal_focus)
+            prop_id, inc_value, prop_name = self.props[self.prop_idx]
+            print(f'{prop_name}: {self.cap.get(prop_id)}')
